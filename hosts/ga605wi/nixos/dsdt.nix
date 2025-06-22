a6cee8699bbbf915e32e8729060ac05e1ee6b266
Rajeev Menon <26593011+rajeevmen0n@users.noreply.github.com>
2025-06-22 14:50:48 -0700

    Finalize DSDT patch module

--- hosts/ga605wi/nixos/dsdt.nix
+++ hosts/ga605wi/nixos/dsdt.nix
@@ -1,7 +1,27 @@
 { pkgs, ... }: let
-  acpiOverrideDrv = pkgs.runCommand "acpi-override" {} ''
-    mkdir -p $out
-    cp ${./dsdt-upgrade.cpio} $out/dsdt_override
+  acpiOverrideDrv = pkgs.runCommand "acpi-override" {
+    nativeBuildInputs = [ pkgs.acpica-tools pkgs.cpio pkgs.patch ];
+  } ''
+    set -euxo pipefail
+
+    mkdir -p $out/kernel/firmware/acpi
+    cd $out
+    cp ${./dsdt.dat} ./dsdt.dat
+
+    # Decompile to DSL
+    iasl -d dsdt.dat
+
+    # Apply patch
+    patch < ${./dsdt.patch}
+
+    # Recompile
+    iasl -tc -ve dsdt.dsl
+
+    # Copy the AML into kernel firmware structure
+    cp dsdt.aml kernel/firmware/acpi/
+
+    # Create CPIO archive
+    find kernel | cpio -H newc --create > dsdt_override
   '';
 in {
   boot.initrd.prepend = [ "${acpiOverrideDrv}/dsdt_override" ];
