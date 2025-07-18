d73d1c09bda032f94c6ce4b55393451479a4ef0d
Rajeev Menon <26593011+rajeevmen0n@users.noreply.github.com>
2025-07-18 16:36:16 -0700

    Add mainframe config

---
+++ hosts/mainframe/configuration.nix
@@ -0,0 +1,15 @@
+let
+  hosts = import ../../config/hosts.nix;
+in {
+  imports = [
+    ./hardware-configuration.nix
+    ../../nixos/server.nix
+    ../../nixos/system/users.nix
+  ];
+
+  networking.hostName = hosts.mainframe.hostname;
+
+  services.openssh.enable = true;
+
+  system.stateVersion = "25.05";
+}
