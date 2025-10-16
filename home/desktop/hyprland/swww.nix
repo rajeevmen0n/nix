69b4f1983bfee1340bfb978855c5abdadf4d23fe
Rajeev Menon <rajeevrmenon@proton.me>
2025-10-16 05:06:02 -0700

    Change wallpaper folder location

--- home/desktop/hyprland/swww.nix
+++ home/desktop/hyprland/swww.nix
@@ -1,11 +1,10 @@
 { pkgs, lib, ... }:
 let
-  users = import ../../../config/users.nix;
   changeWallpaper = pkgs.writeShellScriptBin "change-wallpaper" ''
     if [ -n "$1" ]; then
       WALLPAPER="$1"
     else
-      WALLPAPER=$(find /home/${users.default.username}/Pictures/Wallpapers -type f | shuf -n 1)
+      WALLPAPER=$(find /mnt/files/wallpapers -type f | shuf -n 1)
     fi
     ${pkgs.pywal}/bin/wal -i $WALLPAPER
     ${pkgs.swww}/bin/swww img --transition-type wipe --transition-angle 30 --transition-step 90 $WALLPAPER
