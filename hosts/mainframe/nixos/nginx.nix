9952b7875d4bea2438decf828b9ff7383366e690
Rajeev Menon <26593011+rajeevmen0n@users.noreply.github.com>
2026-08-14 18:27:38 -0700

    refactor(mainframe): refactor nginx configuration with helpers and recommended settings

--- hosts/mainframe/nixos/nginx.nix
+++ hosts/mainframe/nixos/nginx.nix
@@ -1,10 +1,59 @@
 { config, ... }:
+
 let
   domain = "icyfire.dev";
-  headscaleDomain = "headscale.${domain}";
-  headplaneDomain = "headplane.${domain}";
   authDomain = "auth.${domain}";
-  lldapDomain = "ldap.${domain}";
+
+  # Reusable Authelia auth verification endpoint
+  autheliaAuthLocation = {
+    extraConfig = ''
+      internal;
+      proxy_pass http://127.0.0.1:9091/api/authz/auth-request;
+      proxy_set_header X-Original-Method $request_method;
+      proxy_set_header X-Original-URL $scheme://$http_host$request_uri;
+      proxy_set_header X-Forwarded-For $remote_addr;
+      proxy_set_header Content-Length "";
+      proxy_set_header Connection "";
+    '';
+  };
+
+  # Directives to protect a location with Authelia forward-auth
+  autheliaProtectedConfig = ''
+    auth_request /internal/authelia/authz;
+    auth_request_set $target_url $scheme://$http_host$request_uri;
+    error_page 401 =302 https://${authDomain}/?rd=$target_url;
+
+    auth_request_set $user $upstream_http_remote_user;
+    auth_request_set $groups $upstream_http_remote_groups;
+    auth_request_set $name $upstream_http_remote_name;
+    auth_request_set $email $upstream_http_remote_email;
+    proxy_set_header Remote-User $user;
+    proxy_set_header Remote-Groups $groups;
+    proxy_set_header Remote-Name $name;
+    proxy_set_header Remote-Email $email;
+  '';
+
+  # Extra headers for identity / auth endpoints
+  forwardAuthHeaders = ''
+    proxy_set_header X-Forwarded-URI $request_uri;
+    proxy_set_header X-Forwarded-Ssl on;
+  '';
+
+  # Helper for standard SSL-enabled reverse proxy vhosts
+  mkProxyVhost = {
+    proxyPass,
+    proxyWebsockets ? true,
+    extraConfig ? "",
+    extraLocations ? { },
+  }: {
+    forceSSL = true;
+    useACMEHost = domain;
+    locations = {
+      "/" = {
+        inherit proxyPass proxyWebsockets extraConfig;
+      };
+    } // extraLocations;
+  };
 in
 {
   sops.secrets.cloudflare = {};
@@ -18,7 +67,7 @@ in
     acceptTerms = true;
     defaults.email = "admin@icyfire.dev";
     certs."${domain}" = {
-      domain = domain;
+      inherit domain;
       group = "nginx";
       extraDomainNames = [ "*.${domain}" ];
       dnsProvider = "cloudflare";
@@ -30,120 +79,44 @@ in
 
   services.nginx = {
     enable = true;
+    recommendedProxySettings = true;
+    recommendedTlsSettings = true;
+    recommendedOptimisation = true;
+    recommendedGzipSettings = true;
+
     virtualHosts = {
       "default" = {
         default = true;
-        serverName = "_";
-        addSSL = true;
-        useACMEHost = domain;
-        extraConfig = "ssl_reject_handshake on;";
-        locations."/" = {
-          return = "404";
-        };
+        rejectSSL = true;
+        locations."/".return = "404";
       };
 
-      "main" = {
-        serverName = domain;
+      "${domain}" = {
         forceSSL = true;
         useACMEHost = domain;
       };
 
-      "headscale" = {
-        serverName = headscaleDomain;
-        useACMEHost = domain;
-        forceSSL = true;
-        locations."/" = {
-          proxyPass = "http://127.0.0.1:8080";
-          proxyWebsockets = true;
-          extraConfig = ''
-            proxy_set_header Host $host;
-            proxy_set_header X-Real-IP $remote_addr;
-            proxy_set_header X-Forwarded-For $proxy_add_x_forwarded_for;
-            proxy_set_header X-Forwarded-Proto $scheme;
-          '';
+      "headscale.${domain}" = mkProxyVhost {
+        proxyPass = "http://127.0.0.1:8080";
+      };
+
+      "headplane.${domain}" = mkProxyVhost {
+        proxyPass = "http://127.0.0.1:3000";
+        extraConfig = autheliaProtectedConfig;
+        extraLocations = {
+          "/internal/authelia/authz" = autheliaAuthLocation;
+          "= /".return = "302 https://headplane.${domain}/admin";
         };
       };
 
-      "headplane" = {
-        serverName = headplaneDomain;
-        useACMEHost = domain;
-        forceSSL = true;
-        locations."/internal/authelia/authz" = {
-          extraConfig = ''
-            internal;
-            proxy_pass http://127.0.0.1:9091/api/authz/auth-request;
-            proxy_set_header X-Original-Method $request_method;
-            proxy_set_header X-Original-URL $scheme://$http_host$request_uri;
-            proxy_set_header X-Forwarded-For $remote_addr;
-            proxy_set_header Content-Length "";
-            proxy_set_header Connection "";
-          '';
-        };
-        locations."= /" = {
-          return = "302 https://${headplaneDomain}/admin";
-        };
-        locations."/" = {
-          proxyPass = "http://127.0.0.1:3000";
-          proxyWebsockets = true;
-          extraConfig = ''
-            auth_request /internal/authelia/authz;
-            auth_request_set $target_url $scheme://$http_host$request_uri;
-            error_page 401 =302 https://${authDomain}/?rd=$target_url;
-
-            auth_request_set $user $upstream_http_remote_user;
-            auth_request_set $groups $upstream_http_remote_groups;
-            auth_request_set $name $upstream_http_remote_name;
-            auth_request_set $email $upstream_http_remote_email;
-            proxy_set_header Remote-User $user;
-            proxy_set_header Remote-Groups $groups;
-            proxy_set_header Remote-Name $name;
-            proxy_set_header Remote-Email $email;
-
-            proxy_set_header Host $host;
-            proxy_set_header X-Real-IP $remote_addr;
-            proxy_set_header X-Forwarded-For $proxy_add_x_forwarded_for;
-            proxy_set_header X-Forwarded-Proto $scheme;
-            proxy_set_header X-Forwarded-Host $host;
-          '';
-        };
+      "auth.${domain}" = mkProxyVhost {
+        proxyPass = "http://127.0.0.1:9091";
+        extraConfig = forwardAuthHeaders;
       };
 
-      "auth" = {
-        serverName = authDomain;
-        useACMEHost = domain;
-        forceSSL = true;
-        locations."/" = {
-          proxyPass = "http://127.0.0.1:9091";
-          proxyWebsockets = true;
-          extraConfig = ''
-            proxy_set_header Host $host;
-            proxy_set_header X-Real-IP $remote_addr;
-            proxy_set_header X-Forwarded-For $proxy_add_x_forwarded_for;
-            proxy_set_header X-Forwarded-Proto $scheme;
-            proxy_set_header X-Forwarded-Host $host;
-            proxy_set_header X-Forwarded-URI $request_uri;
-            proxy_set_header X-Forwarded-Ssl on;
-          '';
-        };
-      };
-
-      "lldap" = {
-        serverName = lldapDomain;
-        useACMEHost = domain;
-        forceSSL = true;
-        locations."/" = {
-          proxyPass = "http://127.0.0.1:17170";
-          proxyWebsockets = true;
-          extraConfig = ''
-            proxy_set_header Host $host;
-            proxy_set_header X-Real-IP $remote_addr;
-            proxy_set_header X-Forwarded-For $proxy_add_x_forwarded_for;
-            proxy_set_header X-Forwarded-Proto $scheme;
-            proxy_set_header X-Forwarded-Host $host;
-            proxy_set_header X-Forwarded-URI $request_uri;
-            proxy_set_header X-Forwarded-Ssl on;
-          '';
-        };
+      "ldap.${domain}" = mkProxyVhost {
+        proxyPass = "http://127.0.0.1:17170";
+        extraConfig = forwardAuthHeaders;
       };
     };
   };
