0eeb7ab77acf26fc92d7732639e6b3ebdd609e6a
Rajeev Menon <26593011+rajeevmen0n@users.noreply.github.com>
2026-08-14 19:43:34 -0700

    feat(tmux): add reload, pane resize, and vi copy mode keybindings

--- home/cli/tmux.nix
+++ home/cli/tmux.nix
@@ -9,24 +9,39 @@
     extraConfig = ''
       set -g focus-events on
       set -ga terminal-overrides ',*:Ss=\E[%p1%d q:Se=\E[5 q'
+
+      unbind r
+      bind r source-file ~/.tmux.conf
+
+      bind -r j resize-pane -D 5
+      bind -r k resize-pane -U 5
+      bind -r l resize-pane -R 5
+      bind -r h resize-pane -L 5
+
+      bind -r m resize-pane -Z
+
+      bind-key -T copy-mode-vi 'v' send -X begin-selection
+      bind-key -T copy-mode-vi 'y' send -X copy-selection
+
+      unbind -T copy-mode-vi MouseDragEnd1Pane
     '';
     plugins = with pkgs; [
-        tmuxPlugins.vim-tmux-navigator
-        {
-            plugin = tmuxPlugins.catppuccin;
-            extraConfig = ''
-            set -g @catppuccin_flavor 'mocha'
-            set -g @catppuccin_window_status_style "rounded"
-            set -g status-right-length 100
-            set -g status-left-length 100
-            set -g status-left ""
-            set -g status-right "#{E:@catppuccin_status_application}"
-            set -agF status-right "#{E:@catppuccin_status_cpu}"
-            set -ag status-right "#{E:@catppuccin_status_session}"
-            set -ag status-right "#{E:@catppuccin_status_uptime}"
-            set -agF status-right "#{E:@catppuccin_status_battery}"
-            '';
-        }
+      tmuxPlugins.vim-tmux-navigator
+      {
+        plugin = tmuxPlugins.catppuccin;
+        extraConfig = ''
+          set -g @catppuccin_flavor 'mocha'
+          set -g @catppuccin_window_status_style "rounded"
+          set -g status-right-length 100
+          set -g status-left-length 100
+          set -g status-left ""
+          set -g status-right "#{E:@catppuccin_status_application}"
+          set -agF status-right "#{E:@catppuccin_status_cpu}"
+          set -ag status-right "#{E:@catppuccin_status_session}"
+          set -ag status-right "#{E:@catppuccin_status_uptime}"
+          set -agF status-right "#{E:@catppuccin_status_battery}"
+        '';
+      }
     ];
   };
 }
